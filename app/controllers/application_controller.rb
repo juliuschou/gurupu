6d9742ffff4403568322592bac71c9e020026400
wanzi chou <juliuschou@gmail.com>
2013-07-01 20:33:40 +0800

    i18n設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :login?
+  before_filter :set_locale
 
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+  
   private
 
   def current_user
