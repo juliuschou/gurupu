2ccac637d968f0ada4a9619a6f34a8ef8ee3f33d
wanzi chou <juliuschou@gmail.com>
2013-07-22 22:22:46 +0800

    xxx
    xxx

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   provider :facebook, '202286933257865', 'd7b3880da724ded043e17395f8b8ed1e'
 end
